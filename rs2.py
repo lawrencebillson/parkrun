f534862fdb17a5f04fe33769259ead154c5a48ac
lawrencebillson <lawrencebillson@yahoo.com.au>
2015-09-30 09:37:24 +0800

    Delete rs2.py
    Moved under the /www folder

--- rs2.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-
-import sys
-import serial
-serialport = serial.Serial("/dev/ttyUSB0", 9600, timeout=0.5)
-while 1:
-	response = serialport.readlines(None)
-	print response
