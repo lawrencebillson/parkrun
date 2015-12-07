f2c5885ebc41f68ff6719a10562fb613d0a78dbe
Lawrence Billson <None@hotmail.com>
2015-12-07 08:08:13 +0800

    Started work on the upgrade process

---
+++ tmp/prupgrade.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# Script that is automatically run by the parkrun portable app when doing
+# an upgrade. A pretty crude attempt at package management.
+#
+# Lawrence Billson, 2015
+#
+echo "Executing post-install scripts"
+sleep 1
+echo "Completed execution of post-install scripts"
