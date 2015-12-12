6a05b3032201dfda6980be16f6529ed8c93891f1
lawrencebillson <lawrencebillson@yahoo.com.au>
2015-12-12 11:15:14 +0800

    Update readserial.py
    For some reason the stopwatch event had three entries (rather than the normal two). This seemed to fix it.

--- www/cgi-bin/readserial.py
+++ www/cgi-bin/readserial.py
@@ -6,7 +6,7 @@ import serial
 serialport = serial.Serial("/dev/ttyUSB0", 9600, timeout=0.5)
 while 1:
 	response = serialport.readlines(None)
-	if len(response) == 2:
+	if len(response) >= 2:
 		print response[1].encode("hex") 		
 		os._exit(0)
 	
