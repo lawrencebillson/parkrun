b0848335ff694dd3b08391d1ec439f81add669b0
lawrencebillson <Lawrencebillson@gmail.com>
2016-03-24 15:50:08 +0800

    Improvements with auto-install

--- tmp/installpkg.sh
+++ tmp/installpkg.sh
@@ -1,3 +1,7 @@
+#!/bin/sh 
+
+echo *** parkrun portable automatic installer - Lawrence Billson
+echo *** Installing packages from the Internet
 opkg update
 opkg install unzip
 opkg install perl-cgi
@@ -6,3 +10,17 @@ opkg install python-pyserial
 opkg install python-codecs
 opkg install uhttpd
 opkg install kmod-usb-storage block-mount block-hotplug kmod-fs-ext4 kmod-fs-vfat kmod-nls-cp437 kmod-nls-iso8859-1
+#
+echo *** Downloading parkrun portable software from GitHub
+wget -qO- http://github.com/lawrencebillson/parkrun/archive/master.tar.gz | (cd /tmp ; tar -xvzf -)
+# 
+echo *** Installing the Opticon barcode drvier
+opkg install /tmp/parkrun-master/tmp/kmod-usb-serial-opticon_3.18.23-1_ramips_24kec.ipk
+# 
+echo *** Copying the portable parkrun software into its final location
+cd /tmp/parkrun-master ; tar -cf - . | (cd /home/lawrence/target ; tar -xvf -)
+echo *** Done - Rebooting in 20 seconds
+sleep 20
+reboot
+
+
--- tmp/prupgrade.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Script that is automatically run by the parkrun portable app when doing
-# an upgrade. A pretty crude attempt at package management.
-#
-# Lawrence Billson, 2015
-#
-echo "Executing post-install scripts"
-sleep 1
-echo "Completed execution of post-install scripts"
