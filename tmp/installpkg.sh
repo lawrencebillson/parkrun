1582c2b09059caf3d0b5c055258def81d2b30ac9
lawrencebillson <Lawrencebillson@gmail.com>
2016-03-24 14:55:26 +0800

    Developing an installation routine

--- tmp/installpkg.sh
+++ tmp/installpkg.sh
@@ -1,7 +1,8 @@
+opkg update
 opkg install unzip
 opkg install perl-cgi
 opkg install kmod-usb-serial-pl2303
 opkg install python-pyserial
 opkg install python-codecs
 opkg install uhttpd
-
+opkg install kmod-usb-storage block-mount block-hotplug kmod-fs-ext4 kmod-fs-vfat kmod-nls-cp437 kmod-nls-iso8859-1
