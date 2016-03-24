7cf3ba1c33efcebb9323e65b4335edb5bc4475f1
lawrencebillson <Lawrencebillson@gmail.com>
2016-03-24 20:12:52 +0800

    Corrected bug with install package

--- tmp/installpkg.sh
+++ tmp/installpkg.sh
@@ -4,7 +4,7 @@ echo *** parkrun portable automatic installer - Lawrence Billson
 echo *** Installing packages from the Internet
 opkg update
 opkg install unzip
-opkg install perl-cgi
+opkg install perl-www
 opkg install kmod-usb-serial-pl2303
 opkg install python-pyserial
 opkg install python-codecs
@@ -12,7 +12,7 @@ opkg install uhttpd
 opkg install kmod-usb-storage block-mount block-hotplug kmod-fs-ext4 kmod-fs-vfat kmod-nls-cp437 kmod-nls-iso8859-1
 #
 echo *** Downloading parkrun portable software from GitHub
-wget -qO- http://github.com/lawrencebillson/parkrun/archive/master.tar.gz | (cd /tmp ; tar -xvzf -)
+wget --no-check-certificate -qO- http://github.com/lawrencebillson/parkrun/archive/master.tar.gz | (cd /tmp ; tar -xvzf -)
 # 
 echo *** Installing the Opticon barcode drvier
 opkg install /tmp/parkrun-master/tmp/kmod-usb-serial-opticon_3.18.23-1_ramips_24kec.ipk
